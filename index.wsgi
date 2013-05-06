979532bfdea3a9b0e47c1570717d90707d6cbdd9
xiyoulaoyuanjia <xiyoulaoyuanjia@gmail.com>
2013-05-06 15:38:37 +0800

    sae上面搭建好

--- index.wsgi
+++ index.wsgi
@@ -1,3 +1,8 @@
 import sae
+import os
+import sys
+root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(root, 'site-packages/Flask-0.9-py2.7.egg'))
+
 from routes import app
 application = sae.create_wsgi_app(app)
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
+# coding=utf-8
 import os
-import sqlite3
 from flask import Flask, render_template, jsonify,request,url_for,g
 from werkzeug import secure_filename
 
@@ -7,6 +7,8 @@ from vdisksdk import *
 UPLOAD_FOLDER = 'uploadDir/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+
+import MySQLdb as db
 #from getGithubBlog import sqldb
 
 app = Flask(__name__)      
@@ -25,16 +27,44 @@ def allowed_file(filename):
 ### for db 
 @app.before_request
 def before_request():
-	g.db = get_db()
+	### sae has the APP_NAME but local does not
+	### APP_NAME：应用名
+	### APP_VERSION: 当前应用使用的版本号
+	### SERVER_SOFTWARE: 当前server的版本
+	from os import environ
+	if environ.get("SERVER_SOFTWARE", ""):
+		g.db = get_db_sae()
+	else :
+		g.db = get_db()
+
+def get_db_sae():
+	import sae.const
+#	sae.const.MYSQL_DB      # 数据库名
+#	sae.const.MYSQL_USER    # 用户名
+#	sae.const.MYSQL_PASS    # 密码
+#	sae.const.MYSQL_HOST    # 主库域名（可读写）
+#	sae.const.MYSQL_PORT    # 端口，类型为，请根据框架要求自行转换为int
+#	sae.const.MYSQL_HOST_S  # 从库域名（只读）
+	### stupid the port
+	com=db.connect(sae.const.MYSQL_HOST,sae.const.MYSQL_USER,sae.const.MYSQL_PASS,sae.const.MYSQL_DB,charset='utf8',port=int(sae.const.MYSQL_PORT))
+	return com.cursor()
 
 def get_db():
-        com=sqlite3.connect("getGithubBlog/getGithubBlog.db")
-        return com
+	com=db.connect("localhost","root","","blog",charset='utf8',port=3306)
+	return com.cursor()
+#	import sqlite3
+#	com=sqlite3.connect("getGithubBlog/getGithubBlog.db")
+#	return com
 
 def query_db(query, args=(), one=False):
-    cur = g.db.execute(query, args)
-    rv = [dict((cur.description[idx][0], value)
-               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    cur = g.db.execute(query,args)
+	
+#for sqllite3
+#    rv = [dict((cur.description[idx][0], value)
+#               for idx, value in enumerate(row)) for row in cur.fetchall()]
+# for mysql
+    rv = [dict((g.db.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in g.db.fetchall()]
     return (rv[0] if rv else None) if one else rv
 
 @app.teardown_appcontext
@@ -53,7 +83,9 @@ def home():
 ## about blog
 @app.route('/blog/<blogId>')
 def blog(blogId):
-	blog_entries=query_db("select * from blog_entries where id=?",[blogId],one=True)
+#for sqllite3
+#	blog_entries=query_db("select * from blog_entries where id=?",[blogId],one=True)
+	blog_entries=query_db("select * from blog_entries where id=%s",[blogId],one=True)
 	return render_template('blog.html',blog=blog_entries['text'])
 
 
@@ -98,14 +130,7 @@ def upload_file():
 def get_token():
   client = VDiskAPIClient('xiyoulaoyuanjia@gmail.com', 'yuanjia')
   client.post.auth__get_token()
-#  print client.access_token
   return jsonify(token=client.access_token)
-#  return jsonify(result=client.post.auth__get_token())
-  
-
-#  return jsonify(result="ggggggggggg")
-
-
 				
 @app.route('/markdownEditor')
 def markdownEditor():
